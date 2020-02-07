365d1a7bc8258bd6d359fbca16e82dd19820c16c
Heather Shores <Heather@Heathers-MacBook-Air.local>
2020-02-07 11:46:42 -0600

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor amet hoodie vice taxidermy humblebrag. Pinterest
+        bitters fingerstache, roof party lo-fi direct trade cardigan irony
+        pickled locavore. Literally activated charcoal celiac raclette franzen 3
+        wolf moon. YOLO polaroid gentrify meditation cornhole disrupt woke
+        kickstarter sriracha. Stumptown synth gochujang, skateboard pork belly
+        cardigan tumblr chambray asymmetrical. Occupy direct trade before they
+        sold out, tote bag gentrify small batch glossier coloring book shaman
+        seitan. Gastropub raw denim thundercats banh mi.
+      </p>
+    </section>
+  );
+}
